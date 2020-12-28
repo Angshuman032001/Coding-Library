3d6f29d2486b4f4aaf86a86f6b1c34e5cdf8f4ef
Angshuman <angshumanbhowmik03@gmail.com>
2020-12-28 22:56:56 +0530

    update lazy propagation

--- Segment Tree (Lazy Propagation).cpp
+++ Segment Tree (Lazy Propagation).cpp
@@ -1,44 +1,72 @@
-//Credits : invictus_123, kactl, bqi343
-template<class T>struct SegTreeLazy{
-    int n, N; vector <T> tree, lazy;
-    const T unit = 0; //initialize default value
-    void init(int _n) { n = _n, N = 4 * _n; tree.assign(N, unit), lazy.assign(N, unit); }
-    T f(T a, T b){ return (a + b); } //any associative function
-    void prop(int i, int st, int en) {
-        tree[i] += lazy[i];
-        if(st != en) {
-            lazy[2 * i] += lazy[i];
-            lazy[2 * i + 1] += lazy[i];
-        }
-        lazy[i] = 0;
+int arr[N];
+
+template<class T>struct SegmentTreeLazy{
+    int n, N;
+    std::vector<T>tree,lazy;
+
+    /*unit is for query and NOP is for updates*/
+
+    const int NOP = 0; //change according to problem
+    const int unit = inf;//change according to problem
+    
+    void init(int _n){
+        n = _n;
+        N = 4*_n;
+        tree.resize(N);
+        lazy.resize(N);
     }
-    void _update(int i, int st, int en, int l, int r, T val){ //update in the range [l,r]
-        if(lazy[i]) prop(i, st, en);
-        if(en < l || st > r) return;
-        if(l <= st && en <= r) {
-            lazy[i] = val;
-            prop(i, st, en);
+
+    //change operation carefully
+    void operation(int &a, int b){
+        if(b != NOP) a = a + b;
+    }
+
+    //change merge carefully
+    int merge(int a, int b){
+        return min(a, b);
+    }
+
+    void propagate(int i, int l, int r){
+        //cant propagte from leaf node
+        if(l != r){
+            operation(tree[2*i],lazy[i]);
+            operation(tree[2*i+1],lazy[i]);
+            operation(lazy[2*i],lazy[i]);
+            operation(lazy[2*i+1],lazy[i]);
+            lazy[i]=NOP;
+        }
+    }
+
+    void _update(int i, int l, int r, int rx, int ry, int val){
+        propagate(i,l,r);
+        if(rx<=l and r<=ry){
+            operation(tree[i],val);
+            operation(lazy[i],val);
             return;
         }
-        int mid = (st + en) / 2;
-        _update(2 * i, st, mid, l, r, val);
-        _update(2 * i + 1, mid + 1, en, l, r, val);
-        tree[i] = f(tree[2 * i], tree[2 * i + 1]);
+        else if(r<rx or l>ry) return;
+        else{
+            int mid = l + (r - l)/2;
+            _update(2*i, l, mid, rx, ry, val);
+            _update(2*i+1, mid+1, r, rx, ry, val);
+            tree[i] = merge(tree[2*i],tree[2*i+1]);
+        }
     }
-    T _query(int i, int st, int en, int l, int r){ //query in the range [l,r]
-        if(lazy[i]) prop(i, st, en);
-        if(en < l || st > r) return unit;
-        if(l <= st && en <= r) return tree[i];
-        int mid = (st + en) / 2;
-        return f(_query(2 * i, st, mid, l, r), _query(2 * i + 1, mid + 1, en, l, r));
+
+    T _query(int i, int l, int r, int rx, int ry){
+        propagate(i,l,r);
+        if(rx<=l and r<=ry) return tree[i];
+        else if(r<rx || l>ry) return unit;//change unit according to problem
+        else{
+            int m = l+(r-l)/2;
+            return merge(_query(2*i, l, m, rx, ry), _query(2*i+1, m+1, r, rx, ry));
+        }
     }
+
     T query(int l, int r){ return _query(1, 1, n, l, r); }
     void update(int l, int r, T val){ _update(1, 1, n, l, r, val); }
+
 };
-SegTreeLazy<int> seg;
-void add(int l, int r, int x){
-    seg.update(l, r, x);
-}
-int find(int x){
-    return seg.query(x, x);
-}
+
+SegmentTreeLazy<int>seg;
+
